5d8212b21425fb7abb6309f61a8dd8afdeb6e6c4
Kyle G. Christianson <christik@us.ibm.com>
2019-07-17 11:30:06 -0500

    Travis build: 232 (#6)

--- appsody.rb
+++ appsody.rb
@@ -1,41 +1,35 @@
 class Appsody < Formula
-    desc "Appsody command-line interface"
-    homepage "https://github.com/appsody/appsody#readme"
-    version "0.2.4"
-    url "https://github.com/appsody/appsody/releases/download/0.2.4/appsody-homebrew-0.2.4.tar.gz" 
-    sha256 "d8ea67620e80e88ee0cd00807ebe8eb6a4ed730b86b47dddb1920c4458c4ad07"
-  #  url "https://github.com/appsody/appsody/archive/master.zip",
-  #      :tag      => "master"
-  #      :revision => "b5d4f876f4bfe294b70c092210613e30eceb0dc4"
-  #  head "git@github.com:appsody/appsody.git"
-  
-  #  bottle do
+  desc "The Appsody command-line interface"
+  homepage "https://www.appsody.dev"
+  url "https://github.com/appsody/appsody/releases/download/0.2.5/appsody-homebrew-0.2.5.tar.gz"
+  # version "0.2.5"
+  sha256 "44b3f2c1de5e7ee30e0c648f59bdff56215fd123868511458fb5e26278a40788"
+
   def install
     bin.install "appsody"
-    bin.install "appsody-controller" 
+    bin.install "appsody-controller"
     ohai "Checking prerequisites..."
-    #system "docker"
     retval=check_prereqs
-
-      if retval
-        ohai "Done."
-     else
-        opoo "Docker not detected. Please ensure docker is installed and running before using appsody."
-      end
+    if retval
+      ohai "Done."
+    else
+      opoo "Docker not detected. Please ensure docker is installed and running before using appsody."
+    end
   end
+
   def check_prereqs
     begin
       original_stderr = $stderr.clone
       original_stdout = $stdout.clone
-      $stderr.reopen(File.new('/dev/null', 'w'))
-      $stdout.reopen(File.new('/dev/null', 'w'))
+      $stderr.reopen(File.new("/dev/null", "w"))
+      $stdout.reopen(File.new("/dev/null", "w"))
       begin
-        system('/usr/local/bin/docker', 'ps')
+        system("/usr/local/bin/docker", "ps")
         retval=true
       rescue
         retval=false
       end
-    rescue Exception => e
+    rescue => e
       $stdout.reopen(original_stdout)
       $stderr.reopen(original_stderr)
       raise e
@@ -43,12 +37,10 @@ class Appsody < Formula
       $stdout.reopen(original_stdout)
       $stderr.reopen(original_stderr)
     end
-    return retval
-  end  
-
-  
-    test do
-      raise "Test not implemented."
-    end
+    retval
   end
-  
+
+  test do
+    raise "Test not implemented."
+  end
+end
